c4c39844949baccbe7deaa234dcc5f11909abe39
JulissaDantes <julissadcj@gmail.com>
2023-09-23 20:51:10 -0400

    connect ethers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from './components/button';
+import { ethers } from 'ethers';
 import Header from './components/header';
 import Footer from './components/footer';
 import './App.css';
 
 function App() {
-  function handleRedStoneClick() {
+  const { WrapperBuilder } = require("@redstone-finance/evm-connector");
+
+  async function handleRedStoneClick() {
+    const provider = new ethers.WebSocketProvider(window.ethereum);
+    await provider;
+
+    const contract = new ethers.Contract(address, abi, provider);
+
+    const wrappedContract = WrapperBuilder.wrap(contract).usingDataService(
+      {
+        dataFeeds: ["ETH", "BTC"],
+      },
+    );
+
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const signer = provider.getSigner();
+    await signer;
+
+    await wrappedContract.executeYourMethod();
+
+    try {
+      const tx = await contract.connect(signer).getLatestPrice(["ETH", "BTC"]);
+      console.log('tx:',tx);
+    } catch(err) {
+      console.log('failed:', err);
+    }
     console.log("am jereeeeeeee");
   }
 
